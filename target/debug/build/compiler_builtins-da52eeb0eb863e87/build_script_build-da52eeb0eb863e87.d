b3a77d378c17fed5fccc9ddd552e9a6ca53a4ac1
Jeff Zhang <zhang.jeffreyd@gmail.com>
2020-11-17 10:28:00 -0500

    Create kernel that targets x_86_64 bare metal

---
+++ target/debug/build/compiler_builtins-da52eeb0eb863e87/build_script_build-da52eeb0eb863e87.d
@@ -0,0 +1,5 @@
+/Users/jeffzh4ng/Documents/code/rust-os/target/debug/build/compiler_builtins-da52eeb0eb863e87/build_script_build-da52eeb0eb863e87: /Users/jeffzh4ng/.rustup/toolchains/nightly-x86_64-apple-darwin/lib/rustlib/src/rust/vendor/compiler_builtins/build.rs
+
+/Users/jeffzh4ng/Documents/code/rust-os/target/debug/build/compiler_builtins-da52eeb0eb863e87/build_script_build-da52eeb0eb863e87.d: /Users/jeffzh4ng/.rustup/toolchains/nightly-x86_64-apple-darwin/lib/rustlib/src/rust/vendor/compiler_builtins/build.rs
+
+/Users/jeffzh4ng/.rustup/toolchains/nightly-x86_64-apple-darwin/lib/rustlib/src/rust/vendor/compiler_builtins/build.rs:
