72348cb8c35b87f0d8c70401835dd8f7ada6d9a4
Jeff Zhang <zhang.jeffreyd@gmail.com>
2020-11-18 14:24:39 -0500

    Remove target from git

--- target/debug/build/bootloader-608faae2937a7928/build_script_build-608faae2937a7928.d
+++
@@ -1,5 +0,0 @@
-/Users/jeffzh4ng/Documents/code/rust-os/target/debug/build/bootloader-608faae2937a7928/build_script_build-608faae2937a7928: /Users/jeffzh4ng/.cargo/registry/src/github.com-1ecc6299db9ec823/bootloader-0.9.11/build.rs
-
-/Users/jeffzh4ng/Documents/code/rust-os/target/debug/build/bootloader-608faae2937a7928/build_script_build-608faae2937a7928.d: /Users/jeffzh4ng/.cargo/registry/src/github.com-1ecc6299db9ec823/bootloader-0.9.11/build.rs
-
-/Users/jeffzh4ng/.cargo/registry/src/github.com-1ecc6299db9ec823/bootloader-0.9.11/build.rs:
