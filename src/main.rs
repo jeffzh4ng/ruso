d381e55419d8ed796b609da998273cb559c76c9c
Jeff Zhang <zhang.jeffreyd@gmail.com>
2020-12-07 18:52:47 -0500

    Create double fault handler

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,11 @@ pub extern "C" fn _start() -> ! {
 
     rust_os::init();
 
-    x86_64::instructions::interrupts::int3();
+    // x86_64::instructions::interrupts::int3();
+
+    unsafe {
+        *(0xdeadbeef as *mut u64) = 42;
+    };
     
     #[cfg(test)]
     test_main();
