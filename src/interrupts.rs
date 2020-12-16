b531ae7fc16cf90be4d58e9cc90dae432072ebdc
Jeff Zhang <zhang.jeffreyd@gmail.com>
2020-12-16 11:48:02 -0500

    Handle keyboard interrupts - print them to screen

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,12 +1,13 @@
-use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
-use crate::println;
 use crate::gdt;
 use crate::print;
+use crate::println;
 use lazy_static::lazy_static;
-use spin;
 use pic8259_simple::ChainedPics;
+use spin;
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 
-pub static PICS: spin::Mutex<ChainedPics> = spin::Mutex::new(unsafe { ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET) });
+pub static PICS: spin::Mutex<ChainedPics> =
+    spin::Mutex::new(unsafe { ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET) });
 
 pub const PIC_1_OFFSET: u8 = 32;
 pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
@@ -14,17 +15,18 @@ pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
 #[derive(Debug, Clone, Copy)]
 #[repr(u8)]
 pub enum InterruptIndex {
-  Timer = PIC_1_OFFSET
+    Timer = PIC_1_OFFSET,
+    Keyboard,
 }
 
 impl InterruptIndex {
-  fn as_u8(self) -> u8 {
-    self as u8
-  }
-  
-  fn as_usize(self) -> usize {
-    usize::from(self.as_u8())
-  }
+    fn as_u8(self) -> u8 {
+        self as u8
+    }
+
+    fn as_usize(self) -> usize {
+        usize::from(self.as_u8())
+    }
 }
 
 lazy_static! {
@@ -36,35 +38,70 @@ lazy_static! {
       .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX); // catch double faults that arise from kernel stack overflows
     };
     idt[InterruptIndex::Timer.as_usize()].set_handler_fn(timer_interrupt_handler);
+    idt[InterruptIndex::Keyboard.as_usize()].set_handler_fn(keyboard_interrupt_handler);
     idt
   };
 }
 
 pub fn init_idt() {
-  IDT.load();
+    IDT.load();
 }
 
 extern "x86-interrupt" fn breakpoint_handler(stack_frame: &mut InterruptStackFrame) {
-  println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
+    println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
 }
 
-extern "x86-interrupt" fn double_fault_handler(stack_frame: &mut InterruptStackFrame, _error_code: u64) -> ! { // double fault handler is diverging bc x86_64 architecture doesn't permit returning from a double fault exception
-  panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
+extern "x86-interrupt" fn double_fault_handler(
+    stack_frame: &mut InterruptStackFrame,
+    _error_code: u64,
+) -> ! {
+    // double fault handler is diverging bc x86_64 architecture doesn't permit returning from a double fault exception
+    panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
 }
 
 extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: &mut InterruptStackFrame) {
-  print!(".");
+    print!(".");
 
-  unsafe {
-    PICS.lock()
-        .notify_end_of_interrupt(InterruptIndex::Timer.as_u8());
-  }
+    unsafe {
+        PICS.lock()
+            .notify_end_of_interrupt(InterruptIndex::Timer.as_u8());
+    }
+}
+
+extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: &mut InterruptStackFrame) {
+    use pc_keyboard::{layouts, HandleControl, Keyboard, ScancodeSet1, DecodedKey};
+    use spin::Mutex;
+    use x86_64::instructions::port::Port;
+
+    lazy_static! {
+        static ref KEYBOARD: Mutex<Keyboard<layouts::Us104Key, ScancodeSet1>> = Mutex::new(
+            Keyboard::new(layouts::Us104Key, ScancodeSet1, HandleControl::Ignore) // HandleControl allows us to map ctr+[a-z] to Unicode chars
+        );
+    }
+
+    let mut keyboard = KEYBOARD.lock();
+    let mut port = Port::new(0x60);
+
+    let scancode: u8 = unsafe { port.read() };
+    if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
+      if let Some(key) = keyboard.process_keyevent(key_event) {
+        match key {
+          DecodedKey::Unicode(character) => print!("{}", character),
+          DecodedKey::RawKey(key) => print!("{:?}", key)
+        }
+      }
+    }
+
+    unsafe {
+        PICS.lock()
+            .notify_end_of_interrupt(InterruptIndex::Keyboard.as_u8())
+    }
 }
 
 #[test_case]
 fn test_breakpoint_exception() {
-  // triggers breakpoint exception
-  x86_64::instructions::interrupts::int3();
+    // triggers breakpoint exception
+    x86_64::instructions::interrupts::int3();
 
-  // this test tests that execution continues afterwards, and that the kernel's breakpoint IDT handler works correctly
+    // this test tests that execution continues afterwards, and that the kernel's breakpoint IDT handler works correctly
 }
